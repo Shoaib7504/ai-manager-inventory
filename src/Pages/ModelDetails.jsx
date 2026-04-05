a98f6d318fda7d92f889bccac80e01457c9bc809
Md Shoaib Hossain <shoaibhossain188@gmail.com>
2026-04-05 16:09:00 +0600

    added delete model page

--- src/Pages/ModelDetails.jsx
+++ src/Pages/ModelDetails.jsx
@@ -1,15 +1,51 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
-import { Link, useLoaderData } from 'react-router';
+import { Link, Navigate, useLoaderData, useNavigate } from 'react-router';
+import Swal from 'sweetalert2';
 
 const ModelDetails = () => {
 
     const data = useLoaderData()
-    const model=data.result
+    const model = data.result
+const Navigate=useNavigate()
+
+    const handleDelete = () => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:3000/models/${model._id}`, {
+                    method: "DELETE",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                })
+                    .then((res) => res.json())
+                    .then((data) => {
+                        console.log(data);
+                        Navigate("/all-models");
+
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "Your file has been deleted.",
+                            icon: "success",
+                        });
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            }
+        });
+    };
 
     return (
         <div>
-        
+
             <div className="max-w-5xl mx-auto p-4 md:p-6 lg:p-8">
                 <div className="card bg-base-100 shadow-xl border border-gray-200 rounded-2xl overflow-hidden">
                     <div className="flex flex-col md:flex-row gap-8 p-6 md:p-8">
@@ -54,7 +90,7 @@ const ModelDetails = () => {
                                     Download
                                 </button>
                                 <button
-                                    // onClick={handleDlete}
+                                    onClick={handleDelete}
                                     className="btn btn-outline rounded-full border-gray-300 hover:border-pink-500 hover:text-pink-600"
                                 >
                                     Delete
